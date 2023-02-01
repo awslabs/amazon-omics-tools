22004269114e3d0347a164d85f35744fb4be8c8a
Vidul Mahendru <mahvidul@amazon.com>
2023-02-01 17:59:25 -0500

    Revert "Add URI parser for omics resource URIs"
    This reverts commit 23ee40d1f42ed90477cd5752ca542fc2aca81aac.

--- tests/omics_uri_agent/__init__.py
+++
@@ -1,2 +0,0 @@
-VALID_READSET_URI = "omics://123412341234.storage.us-east-1.amazonaws.com/5432154321/readSet/5346184667/source1"
-VALID_REFERENCE_URI = "omics://123412341234.storage.us-east-1.amazonaws.com/5432154321/reference/5346184667/source"
