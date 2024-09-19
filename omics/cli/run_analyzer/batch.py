f2f964400b79d0bc0fa1e697a425a5d9be6d503f
Mark Schreiber <mrschre@amazon.com>
2024-09-19 13:22:50 -0400

    continued work on aggregation of run batches

--- omics/cli/run_analyzer/batch.py
+++ omics/cli/run_analyzer/batch.py
@@ -1,7 +1,8 @@
 import sys
+import statistics
 
 from . import __main__ as main
-
+from . import utils
 
 hdrs = [
             "type",
@@ -10,25 +11,21 @@ hdrs = [
             "meanRunningSeconds",
             "maximumRunningSeconds",
             "stdDevRunningSeconds",
-            "maximumPeakCpuUtilization",
-            "maximumPeakMemoryUtilization",
-            "maxGpusReserved",
+            "maximumCpuUtilization",
+            "maximumMemoryUtilization",
+            "maximumGpusReserved",
             "recommendedCpus",
             "recommendedMemoryGiB",
             "recommendOmicsInstanceType",
-            "estimatedUSDForMaximumRunningSeconds",
-            "estimatedUSDForMeanRunningSeconds",
-            "storageMaximumGiB",
-            "meanStorageMaximumGiB",
-            "stdDevStorageMaximumGiB"
+            "maximumEstimatedUSD",
+            "meanEstimatedUSD"
         ]
 
-wdl_task_regex = r"^([^-]+)(-\d+-\d+.*)?$"
-nextflow_task_regex = r"^(.+)(\s\(.+\))$"
-cwl_task_regex = r"^(^\D+)(_\d+)?$"
-
-def aggregate_and_print(resources_list, pricing, headroom=0.0, out=sys.stdout):
+def aggregate_and_print(resources_list, pricing, engine, headroom=0.0, out=sys.stdout):
     """Aggregate resources and print to output"""
+    if engine not in utils.ENGINES:
+        raise ValueError(f"Invalid engine for use in batch aggregation: {engine}. Must be one of {utils.ENGINES}")
+
     for resources in resources_list:        
         # filter resources to remove anything where res["type"] is not "run"
         resources = [r for r in resources if r["type"] == "run"]
@@ -36,7 +33,59 @@ def aggregate_and_print(resources_list, pricing, headroom=0.0, out=sys.stdout):
             main.add_metrics(res, resources, pricing, headroom)
 
     # if there are resources from the runs with a common name or prefix then aggregate
-    names = [r["name"] for r in resources]
+    names = [utils.task_base_name(r["name"]) for r in resources]
     names = list(set(names))
+    names.sort()
+
+    # print hdrs
+    print(", ".join(hdrs), file=out)
+
     for name in names:
-        _aggregate_resources(resources, name)
+        _aggregate_resources(resources, name, out)
+
+def _aggregate_resources(resources, name, out):
+    """Aggregate resources with the same name"""
+    filtered = [r for r in resources if utils.task_base_name(r["name"]) == name]
+    if filtered:
+        res = filtered[0]
+        for k in hdrs:
+            if k in ["type", "name"]:
+                continue
+            elif k == "count":
+                res[k] = _do_aggregation(filtered, k, "count")
+            elif k.startswith("mean"):
+                # resource key is k with "mean" removed and the first char to lowercase
+                rk = k.replace("mean", "")[0].lower() + k.replace("mean", "")[1:]
+                res[k] = _do_aggregation(filtered, rk, "mean")
+            elif k.startswith("stdDev"):
+                rk = k.replace("stdDev", "")[0].lower() + k.replace("stdDev", "")[1:]
+                res[k] = _do_aggregation(filtered, rk, "stdDev")
+            elif k.startswith("maximum"):
+                rk = k.replace("maximum", "")[0].lower() + k.replace("maximum", "")[1:]
+                res[k] = _do_aggregation(filtered, rk, "maximum")
+            elif k in ["recommendedCpus", "recommendedMemoryGiB"]:
+                _do_aggregation(resources, k, "maximum")
+            elif k in ["recommendOmicsInstanceType"]:
+                _do_aggregation(resources, "omicsInstanceTypeMinimum", "maximum")
+            else:
+                raise ValueError(f"Unhandled aggregation for key: {k}")
+
+        print(",".join([str(res.get(h, "")) for h in hdrs]), file=out)
+
+def _do_aggregation(resources, resource_key, operation):
+    if operation == "count":
+        return len(resources)
+    elif operation == "sum":
+        return sum([r[resource_key] for r in resources])
+    elif operation == "maximum":
+        if resource_key == "omicsInstanceTypeMinimum":
+            # special case for instance types
+            return max(resources, key=lambda x: utils.omics_instance_weight(x[resource_key]))
+        return max([r[resource_key] for r in resources])
+    elif operation == "mean":
+        return round(statistics.mean([r[resource_key] for r in resources]), 2)
+    elif operation == "stdDev":
+        return round(statistics.stdev([r[resource_key] for r in resources]), 2)
+    else:
+        raise ValueError(f"Invalid aggregation operation: {operation}")
+    
---
+++ omics/cli/run_analyzer/utils.py
@@ -0,0 +1,56 @@
+import re
+
+ENGINES = ['WDL', 'CWL', 'Nextflow']
+
+_wdl_task_regex = r"^([^-]+)(-\d+-\d+.*)?$"
+_nextflow_task_regex = r"^(.+)(\s\(.+\))$"
+_cwl_task_regex = r"^(^\D+)(_\d+)?$"
+
+def get_engine(workflow_arn, session) -> str:
+        omics = session.client("omics")
+        id = workflow_arn.split('/')[-1]
+        return omics.get_workflow(id)['engine']
+
+def task_base_name(name: str, engine: str) -> str:
+        # WDL
+        if engine == 'WDL':
+            m = re.match(_wdl_task_regex, name)
+            if m:
+                return m.group(1)
+        # Nextflow
+        elif engine == 'Nextflow':
+            m = re.match(_nextflow_task_regex, name)
+            if m:
+                return m.group(1)
+        # CWL
+        elif engine == 'CWL':
+            m = re.match(_cwl_task_regex, name)
+            if m:
+                return m.group(1)
+        else:
+            raise ValueError(f"Unsupported engine: {engine}")
+        return name
+
+def omics_instance_weight(instance: str) -> int:
+    
+    sizes = {
+        "": 2,
+        "x": 4,
+        "2x": 8,
+        "4x": 16,
+        "8x": 32,
+        "12x": 48,
+        "16x": 64,
+        "24x": 96,
+    }
+    families = {"c": 2, "m": 4, "r": 8, "g4dn": 16, "g5": 32}
+    # remove the "omics." from the string
+    instance.replace("omics.", "")
+    # split the instance into family and size
+    parts = instance.split(".")
+    fam = parts[0]
+    size = parts[1] if len(parts) > 1 else ""
+
+    ccount = sizes[size]
+    weight = ccount * families[fam]
+    return weight
