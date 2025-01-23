ca1dfddd50f6287f81579488d2173a3ad2518d82
Mark Schreiber <mrschre@amazon.com>
2025-01-23 14:53:59 -0500

    redirect amazon-omics-tools to aws-healthomics-tools

---
+++ omics/__init__.py
@@ -0,0 +1,9 @@
+import warnings
+from omics import *
+
+warnings.warn(
+    "This package has been renamed to 'aws-healthomics-tools'. "
+    "Please update your dependencies to use the new package name.",
+    DeprecationWarning,
+    stacklevel=2
+)
